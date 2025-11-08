53f4ed34f1416fb1ebcf09c70a62bc2c7c3585bf
Kyle Beggs <beggskw@gmail.com>
2025-11-08 09:18:54 -0500

    restore to previous state

--- test/isinside.jl
+++ test/isinside.jl
@@ -10,12 +10,7 @@ end
 
 @testset "3D" begin
     boundary = PointBoundary(joinpath(@__DIR__, "data", "box.stl"))
-    display(boundingbox(boundary))
     @test isinside(Point(0.5, 0.5, 0.5), boundary)
-    for p in [Point(0.0, 0.5, 0.5), Point(1.0, 0.5, 0.5), Point(0.5, 0.0, 0.5), Point(0.5, 1.0, 0.5), Point(0.5, 0.5, 0.0), Point(0.5, 0.5, 1.0)]
-        @test isinside(p, boundary)
-    end
-    @test isinside(Point(10.0, 10.0, 10.0), boundary)
     @test !isinside(Point(0.5, 0.5, -0.5), boundary)
     @test !isinside(Point(0.5, 0.5, -0.001), boundary)
 end
